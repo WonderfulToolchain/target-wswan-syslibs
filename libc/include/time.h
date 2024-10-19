b8c9d639be6510faac079693e8456d54b974e3a6
Adrian Siekierka <kontakt@asie.pl>
2024-10-19 08:49:55 +0200

    libc: introduce time.h header

---
+++ libc/include/time.h
@@ -0,0 +1,40 @@
+/**
+ * wonderful-i8086 libc
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * wonderful-i8086 libc has waived all copyright and related or neighboring rights
+ * to wonderful-i8086 libc.
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#ifndef __WF_LIBC_TIME_H__
+#define __WF_LIBC_TIME_H__
+
+#include <stdint.h>
+#include <wonderful.h>
+
+typedef uint32_t clock_t;
+typedef uint32_t time_t;
+
+#define CLOCKS_PER_SEC 75
+
+struct timespec {
+    time_t tv_sec;
+    long tv_nsec;
+};
+
+struct tm {
+    int tm_sec;   /* 0-60 */
+    int tm_min;   /* 0-59 */
+    int tm_hour;  /* 0-23 */
+    int tm_mday;  /* 1-31 */
+    int tm_mon;   /* 0-11 */
+    int tm_year;  /* years since 1900 */
+    int tm_wday;  /* 0-6 */
+    int tm_yday;  /* 0-365 */
+    int tm_isdst;
+};
+
+#endif /* __WF_LIBC_TIME_H__ */
