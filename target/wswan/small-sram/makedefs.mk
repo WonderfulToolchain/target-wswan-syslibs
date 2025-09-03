e605376d30fabf3a0b46b6b426f3d226d15f9c76
Adrian Siekierka <kontakt@asie.pl>
2025-09-03 17:26:50 +0200

    target: support __wf_iram variable declarations in wswan/-sram targets

--- target/wswan/small-sram/makedefs.mk
+++ target/wswan/small-sram/makedefs.mk
@@ -4,7 +4,7 @@ WF_SUBTARGET := small-sram
 include $(dir $(lastword $(MAKEFILE_LIST)))../makedefs-common.mk
 
 WF_TARGET_DIR ?= $(WF)/target/wswan/small-sram
-WF_ARCH_CFLAGS += -mcmodel=small -mno-callee-assume-ss-data-segment -msegelf
+WF_ARCH_CFLAGS += -mcmodel=small -mno-callee-assume-ss-data-segment -msegelf -mstack-declarations
 WF_ARCH_LDFLAGS += -L$(WF)/toolchain/gcc-ia16-elf/lib/v30mz-segelf-separate-stack/small
 WF_ARCH_LIBDIRS += local/$(WF_TARGET_DIR) thirdparty/$(WF_TARGET_DIR) $(WF_TARGET_DIR)
 WF_CRT0 := $(WF_TARGET_DIR)/lib/crt0.o
